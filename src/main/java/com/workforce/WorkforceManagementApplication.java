d1c4577edc922670c57099a735eecce4ddcf7d67
YanislavD <jnislav.dragiiski02@gmail.com>
2026-01-22 21:42:41 +0100

    Create the project

---
+++ src/main/java/com/workforce/WorkforceManagementApplication.java
@@ -0,0 +1,14 @@
+package com.workforce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class WorkforceManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkforceManagementApplication.class, args);
+    }
+}
